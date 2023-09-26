f8d113b77d4f836382b827e10ea6f80445ccb478
15712 <mukesh_mp@geojit.com>
2023-09-26 10:37:09 +0530

    design

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mathq/login/login_controller.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var loginController = Get.put(LoginController());
+
     return Scaffold(
         body: Container(
             width: double.maxFinite,
@@ -18,43 +23,89 @@ class LoginScreen extends StatelessWidget {
                   Color.fromARGB(255, 98, 7, 118),
                   Color.fromARGB(255, 135, 19, 158)
                 ])),
-            child: const Center(
+            child: Center(
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                  Text(
-                    "Sign In",
-                    style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: width * .65,
+                        child: ElevatedButton(
+                          style: const ButtonStyle(
+                              shadowColor:
+                                  MaterialStatePropertyAll(Colors.black),
+                              backgroundColor: MaterialStatePropertyAll(
+                                  Color.fromARGB(255, 115, 24, 132)),
+                              elevation: MaterialStatePropertyAll(10),
+                              shape: MaterialStatePropertyAll(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(20))))),
+                          onPressed: () {},
+                          child: const Text(
+                            "Sign In",
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  TextField(
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.purple,
-                        hintText: "Username",
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20)))),
+                  Obx(
+                    () => Visibility(
+                      visible: loginController.signInOptional.value == true
+                          ? true
+                          : false,
+                      child: const Column(
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.purple,
+                                hintText: "Username",
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20)))),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                                hintText: "Password",
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                                filled: true,
+                                fillColor: Colors.purple),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  TextField(
-                    decoration: InputDecoration(
-                        hintText: "Password",
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        filled: true,
-                        fillColor: Colors.purple),
+                  SizedBox(
+                    width: width * .65,
+                    child: ElevatedButton(
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromARGB(255, 115, 24, 132)),
+                          elevation: MaterialStatePropertyAll(10),
+                          shape: MaterialStatePropertyAll(
+                              RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20))))),
+                      onPressed: () {},
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
                   ),
-                  Text(
-                    "SignUp",
-                    style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500),
-                  )
                 ]))));
   }
 }
