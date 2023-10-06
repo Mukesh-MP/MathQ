14c0830b0bebe1f896544997ac8197db1dfa91a5
15712 <mukesh_mp@geojit.com>
2023-10-06 11:06:50 +0530

    changes userselected

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -94,7 +94,7 @@ class HomeController extends GetxController {
       return Colors.green;
     } else if (answerListAll[questionIndex.value][index].userselected.value ==
         "Y") {
-      return Color.fromARGB(255, 249, 240, 58);
+      return const Color.fromARGB(255, 249, 240, 58);
     } else {
       return Colors.grey.shade200;
     }
@@ -122,9 +122,9 @@ class HomeController extends GetxController {
       // batch.execute('DROP TABLE IF EXISTS answers ');
       // batch.execute('DROP TABLE IF EXISTS questions ');
       batch.execute(
-          'CREATE TABLE questions (qid INTEGER PRIMARY KEY, question TEXT, checked INTEGER');
+          'CREATE TABLE questions (qid INTEGER PRIMARY KEY, question TEXT, checked INTEGER)');
       batch.execute(
-          'CREATE TABLE answers (answerid INTEGER PRIMARY KEY, answertext TEXT,qid INTEGER, correct TEXT)');
+          'CREATE TABLE answers (answerid INTEGER PRIMARY KEY, answertext TEXT,qid INTEGER, correct TEXT,userselected TEXT)');
       // await db.execute(
       //     'CREATE TABLE questions (qid INTEGER PRIMARY KEY, question TEXT)');
       // await db.execute(
@@ -169,9 +169,8 @@ class HomeController extends GetxController {
       });
     }
 
-   // List<Questions> questionisAvailable = await retrieveQuestions();
+    // List<Questions> questionisAvailable = await retrieveQuestions();
 
-    
     // Define a function that inserts dogs into the database
 
     // if (questionisAvailable.isNotEmpty) {
@@ -200,38 +199,36 @@ class HomeController extends GetxController {
       }
     }
 
-  //  List<Settings> settingsAll = await retrieveSettings();
+    //  List<Settings> settingsAll = await retrieveSettings();
 
+    var quesion1 = Questions(
+        qid: 1,
+        question: 'Which of the following values is NOT equal to 34(58+9)?',
+        checked: 0.obs);
+    var quesion2 =
+        Questions(qid: 2, question: 'What is 6% Equals to', checked: 0.obs);
+    var quesion3 = Questions(
+        qid: 3,
+        question: 'How Many Years are there in a Decade?',
+        checked: 0.obs);
+    var quesion4 = Questions(
+        qid: 4, question: 'How Many Months Make a Century?', checked: 0.obs);
+    var quesion5 = Questions(
+        qid: 5,
+        question:
+            'Priya had 16 Red Balls, 2 Green Balls, 9  Blue Balls, and 1 Multicolor Ball. If He Lost 9 Red Balls, 1 Green Ball, and 3 Blue Balls. How Many Balls would be Left?',
+        checked: 0.obs);
 
-      var quesion1 = Questions(
-          qid: 1,
-          question: 'Which of the following values is NOT equal to 34(58+9)?',
-          checked: 0.obs);
-      var quesion2 =
-          Questions(qid: 2, question: 'What is 6% Equals to', checked: 0.obs);
-      var quesion3 = Questions(
-          qid: 3,
-          question: 'How Many Years are there in a Decade?',
-          checked: 0.obs);
-      var quesion4 = Questions(
-          qid: 4, question: 'How Many Months Make a Century?', checked: 0.obs);
-      var quesion5 = Questions(
-          qid: 5,
-          question:
-              'Priya had 16 Red Balls, 2 Green Balls, 9  Blue Balls, and 1 Multicolor Ball. If He Lost 9 Red Balls, 1 Green Ball, and 3 Blue Balls. How Many Balls would be Left?',
-          checked: 0.obs);
-
-      try {
-        await insertQuestions(quesion1);
-        await insertQuestions(quesion2);
-        await insertQuestions(quesion3);
-        await insertQuestions(quesion4);
-        await insertQuestions(quesion5);
-      } catch (e) {
-        showToast(msg: "Question fetching failed");
-        // isloading.value = false;
-      }
-    
+    try {
+      await insertQuestions(quesion1);
+      await insertQuestions(quesion2);
+      await insertQuestions(quesion3);
+      await insertQuestions(quesion4);
+      await insertQuestions(quesion5);
+    } catch (e) {
+      showToast(msg: "Question fetching failed");
+      // isloading.value = false;
+    }
 
     questionsList = await retrieveQuestions();
     CustomLog.customprint(questionsList.toString());
@@ -252,158 +249,157 @@ class HomeController extends GetxController {
       }
     }
 
+    var answer1 = Answers(
+        answerid: 1,
+        answertext: "0.06",
+        qid: 2,
+        correct: 'Y',
+        userselected: 'N'.obs);
+    var answer2 = Answers(
+        answerid: 2,
+        answertext: "0.6",
+        qid: 2,
+        correct: 'N',
+        userselected: 'N'.obs);
+    var answer3 = Answers(
+        answerid: 3,
+        answertext: "0.006",
+        qid: 2,
+        correct: 'N',
+        userselected: 'N'.obs);
+    var answer4 = Answers(
+        answerid: 4,
+        answertext: "0.0006",
+        qid: 2,
+        correct: 'N',
+        userselected: 'N'.obs);
 
-      var answer1 = Answers(
-          answerid: 1,
-          answertext: "0.06",
-          qid: 2,
-          correct: 'Y',
-          userselected: 'N'.obs);
-      var answer2 = Answers(
-          answerid: 2,
-          answertext: "0.6",
-          qid: 2,
-          correct: 'N',
-          userselected: 'N'.obs);
-      var answer3 = Answers(
-          answerid: 3,
-          answertext: "0.006",
-          qid: 2,
-          correct: 'N',
-          userselected: 'N'.obs);
-      var answer4 = Answers(
-          answerid: 4,
-          answertext: "0.0006",
-          qid: 2,
-          correct: 'N',
-          userselected: 'N'.obs);
+    var answer5 = Answers(
+        answerid: 5,
+        answertext: "34 * 67",
+        qid: 1,
+        correct: 'N',
+        userselected: 'N'.obs);
+    var answer6 = Answers(
+        answerid: 6,
+        answertext: "58(34+9)",
+        qid: 1,
+        correct: 'Y',
+        userselected: 'N'.obs);
+    var answer7 = Answers(
+        answerid: 7,
+        answertext: "34 * 58 + 34 * 9",
+        qid: 1,
+        correct: 'N',
+        userselected: 'N'.obs);
+    var answer8 = Answers(
+        answerid: 8,
+        answertext: "1,972 + 306",
+        qid: 1,
+        correct: 'N',
+        userselected: 'N'.obs);
 
-      var answer5 = Answers(
-          answerid: 5,
-          answertext: "34 * 67",
-          qid: 1,
-          correct: 'N',
-          userselected: 'N'.obs);
-      var answer6 = Answers(
-          answerid: 6,
-          answertext: "58(34+9)",
-          qid: 1,
-          correct: 'Y',
-          userselected: 'N'.obs);
-      var answer7 = Answers(
-          answerid: 7,
-          answertext: "34 * 58 + 34 * 9",
-          qid: 1,
-          correct: 'N',
-          userselected: 'N'.obs);
-      var answer8 = Answers(
-          answerid: 8,
-          answertext: "1,972 + 306",
-          qid: 1,
-          correct: 'N',
-          userselected: 'N'.obs);
+    var answer9 = Answers(
+        answerid: 9,
+        answertext: "5 Years",
+        qid: 3,
+        correct: 'N',
+        userselected: 'N'.obs);
+    var answer10 = Answers(
+        answerid: 10,
+        answertext: "10 Years",
+        qid: 3,
+        correct: 'Y',
+        userselected: 'N'.obs);
+    var answer11 = Answers(
+        answerid: 11,
+        answertext: "15 Years",
+        qid: 3,
+        correct: 'N',
+        userselected: 'N'.obs);
+    var answer12 = Answers(
+        answerid: 12,
+        answertext: "20 Years",
+        qid: 3,
+        correct: 'N',
+        userselected: 'N'.obs);
 
-      var answer9 = Answers(
-          answerid: 9,
-          answertext: "5 Years",
-          qid: 3,
-          correct: 'N',
-          userselected: 'N'.obs);
-      var answer10 = Answers(
-          answerid: 10,
-          answertext: "10 Years",
-          qid: 3,
-          correct: 'Y',
-          userselected: 'N'.obs);
-      var answer11 = Answers(
-          answerid: 11,
-          answertext: "15 Years",
-          qid: 3,
-          correct: 'N',
-          userselected: 'N'.obs);
-      var answer12 = Answers(
-          answerid: 12,
-          answertext: "20 Years",
-          qid: 3,
-          correct: 'N',
-          userselected: 'N'.obs);
+    var answer13 = Answers(
+        answerid: 13,
+        answertext: "12",
+        qid: 4,
+        correct: 'N',
+        userselected: 'N'.obs);
+    var answer14 = Answers(
+        answerid: 14,
+        answertext: "120",
+        qid: 4,
+        correct: 'N',
+        userselected: 'N'.obs);
+    var answer15 = Answers(
+        answerid: 15,
+        answertext: "1200",
+        qid: 4,
+        correct: 'Y',
+        userselected: 'N'.obs);
+    var answer16 = Answers(
+        answerid: 16,
+        answertext: "12000",
+        qid: 4,
+        correct: 'N',
+        userselected: 'N'.obs);
 
-      var answer13 = Answers(
-          answerid: 13,
-          answertext: "12",
-          qid: 4,
-          correct: 'N',
-          userselected: 'N'.obs);
-      var answer14 = Answers(
-          answerid: 14,
-          answertext: "120",
-          qid: 4,
-          correct: 'N',
-          userselected: 'N'.obs);
-      var answer15 = Answers(
-          answerid: 15,
-          answertext: "1200",
-          qid: 4,
-          correct: 'Y',
-          userselected: 'N'.obs);
-      var answer16 = Answers(
-          answerid: 16,
-          answertext: "12000",
-          qid: 4,
-          correct: 'N',
-          userselected: 'N'.obs);
+    var answer17 = Answers(
+        answerid: 17,
+        answertext: "15",
+        qid: 5,
+        correct: 'Y',
+        userselected: 'N'.obs);
+    var answer18 = Answers(
+        answerid: 18,
+        answertext: "11",
+        qid: 5,
+        correct: 'N',
+        userselected: 'N'.obs);
+    var answer19 = Answers(
+        answerid: 19,
+        answertext: "28",
+        qid: 5,
+        correct: 'N',
+        userselected: 'N'.obs);
+    var answer20 = Answers(
+        answerid: 20,
+        answertext: "39",
+        qid: 5,
+        correct: 'N',
+        userselected: 'N'.obs);
 
-      var answer17 = Answers(
-          answerid: 17,
-          answertext: "15",
-          qid: 5,
-          correct: 'Y',
-          userselected: 'N'.obs);
-      var answer18 = Answers(
-          answerid: 18,
-          answertext: "11",
-          qid: 5,
-          correct: 'N',
-          userselected: 'N'.obs);
-      var answer19 = Answers(
-          answerid: 19,
-          answertext: "28",
-          qid: 5,
-          correct: 'N',
-          userselected: 'N'.obs);
-      var answer20 = Answers(
-          answerid: 20,
-          answertext: "39",
-          qid: 5,
-          correct: 'N',
-          userselected: 'N'.obs);
+    try {
+      await insertAnswers(answer1);
+      await insertAnswers(answer2);
+      await insertAnswers(answer3);
+      await insertAnswers(answer4);
+      await insertAnswers(answer5);
+      await insertAnswers(answer6);
+      await insertAnswers(answer7);
+      await insertAnswers(answer8);
+      await insertAnswers(answer9);
+      await insertAnswers(answer10);
+      await insertAnswers(answer11);
+      await insertAnswers(answer12);
+      await insertAnswers(answer13);
+      await insertAnswers(answer14);
+      await insertAnswers(answer15);
+      await insertAnswers(answer16);
+      await insertAnswers(answer17);
+      await insertAnswers(answer18);
+      await insertAnswers(answer19);
+      await insertAnswers(answer20);
+    } catch (e) {
+      showToast(msg: "Answer fetch error");
+      // isloading.value = false;
+    }
 
-      try {
-        await insertAnswers(answer1);
-        await insertAnswers(answer2);
-        await insertAnswers(answer3);
-        await insertAnswers(answer4);
-        await insertAnswers(answer5);
-        await insertAnswers(answer6);
-        await insertAnswers(answer7);
-        await insertAnswers(answer8);
-        await insertAnswers(answer9);
-        await insertAnswers(answer10);
-        await insertAnswers(answer11);
-        await insertAnswers(answer12);
-        await insertAnswers(answer13);
-        await insertAnswers(answer14);
-        await insertAnswers(answer15);
-        await insertAnswers(answer16);
-        await insertAnswers(answer17);
-        await insertAnswers(answer18);
-        await insertAnswers(answer19);
-        await insertAnswers(answer20);
-      } catch (e) {
-        showToast(msg: "Answer fetch error");
-        // isloading.value = false;
-      }
-    
     Future<List<Answers>> retrieveAnswers() async {
       // Get a reference to the database.
       final db = await database;
